883f402c49e746cf01d5ea1ca1fb47c7c63e515f
Sandeep <>
2025-03-10 12:56:50 +0530

    HELLO DONE

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
-const root = 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1", {}, "Namaste Hariom");
+// const heading2 = React.createElement("h1", {}, "Namaste Hariom");
+const heading2 = React.createElement("h1", { id: "hello", key: "h1"}, "Namaste Hariom Kumar");  
+// const heading = <h1>Namaste Hariom</h1>; 
+
+//todo jsx => React.createElement() => OBJECT => DOM
+const heading = (
+    <h1 id="hello" className="hello2">
+        Namaste Hariom Kumar wertyuil
+    </h1>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
